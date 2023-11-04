8626e544b5957e07400aae2146935ecc2e03e489
shun0921 <i.mafbp091221@gmail.com>
2023-11-04 22:20:41 +0900

    tutorial 2.1 Youtube画面作成

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_tutorial/animation/animation_page.dart';
 import 'package:flutter_tutorial/building/building_layout_page.dart';
+import 'package:flutter_tutorial/youtube/youtube_page.dart';
 
 class IndexPage extends StatelessWidget {
 
@@ -30,6 +32,17 @@ class IndexPage extends StatelessWidget {
                   );
                 },
                 child: Text('Tutorial1.1'),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed:(){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => YoutubePage()),
+                );
+              },
+              child: Text('YouTubeボタン'),
             ),
           ),
         ],
