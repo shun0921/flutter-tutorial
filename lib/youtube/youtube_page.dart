e605a4d32ff63d92806a8b69ddb83b5d536d3af9
shun0921 <i.mafbp091221@gmail.com>
2023-11-08 11:24:30 +0900

    tutorial3-1 YoutubePageに動的データを追加

--- lib/youtube/youtube_page.dart
+++ lib/youtube/youtube_page.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class MovieInfo {
+  final String imagePath;
+  final String iconPath;
+  final String title;
+  final String subTitle;
+
+  MovieInfo({
+    required this.imagePath,
+    required this.iconPath,
+    required this.title,
+    required this.subTitle,
+  });
+}
+
 class YoutubePage extends StatefulWidget {
   const YoutubePage({Key? key}) : super(key: key);
 
@@ -9,6 +23,22 @@ class YoutubePage extends StatefulWidget {
 
 class _YoutubePageState extends State<YoutubePage> {
   int _selectedIndex = 0;
+
+  final List<MovieInfo> _MovieData = [
+    MovieInfo(
+      imagePath:
+          'https://cgsc.info/cgsc2020/wp-content/uploads/2020/12/image.png',
+      iconPath: 'Icons.circle',
+      title: 'testtesttesttesttesttest',
+      subTitle: '嵐の動画のはず',
+    ),
+    MovieInfo(
+        imagePath:
+            'https://www.chipublib.org/wp-content/uploads/sites/3/2022/09/36079964425_7b3042d5e1_k.jpg',
+        iconPath: 'Icons.circle',
+        title: 'musicmusicmusic',
+        subTitle: 'this is Eminem'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -75,111 +105,108 @@ class _YoutubePageState extends State<YoutubePage> {
         ),
       ),
       body: Container(
-        color: Colors.black87,
-        child: ListView(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: 7,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3.2,
-                      mainAxisSpacing: 8.0,
-                      crossAxisSpacing: 12.0,
+        color: Colors.black,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.black87,
+                child: Column(
+                  children: [
+                    GridView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: 7,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 3.2,
+                        mainAxisSpacing: 8.0,
+                        crossAxisSpacing: 12.0,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        List<CustomButton> buttons = [
+                          CustomButton(
+                              icon: Icons.local_fire_department,
+                              label: "急上昇",
+                              color: Colors.red),
+                          CustomButton(
+                              icon: Icons.music_note,
+                              label: "音楽",
+                              color: Colors.teal),
+                          CustomButton(
+                              icon: Icons.gamepad_rounded,
+                              label: "ゲーム",
+                              color: Colors.pinkAccent),
+                          CustomButton(
+                              icon: Icons.menu_sharp,
+                              label: "ニュース",
+                              color: Colors.indigo),
+                          CustomButton(
+                              icon: Icons.menu_sharp,
+                              label: "学び",
+                              color: Colors.green),
+                          CustomButton(
+                              icon: Icons.live_tv,
+                              label: "ライブ",
+                              color: Colors.deepOrangeAccent),
+                          CustomButton(
+                              icon: Icons.play_circle_fill,
+                              label: "スポーツ",
+                              color: Colors.cyan),
+                        ];
+                        return buttons[index];
+                      },
                     ),
-                    itemBuilder: (BuildContext context, int index) {
-                      List<CustomButton> buttons = [
-                        CustomButton(
-                            icon: Icons.local_fire_department,
-                            label: "急上昇",
-                            color: Colors.red),
-                        CustomButton(
-                            icon: Icons.music_note,
-                            label: "音楽",
-                            color: Colors.teal),
-                        CustomButton(
-                            icon: Icons.gamepad_rounded,
-                            label: "ゲーム",
-                            color: Colors.pinkAccent),
-                        CustomButton(
-                            icon: Icons.menu_sharp,
-                            label: "ニュース",
-                            color: Colors.indigo),
-                        CustomButton(
-                            icon: Icons.menu_sharp,
-                            label: "学び",
-                            color: Colors.green),
-                        CustomButton(
-                            icon: Icons.live_tv,
-                            label: "ライブ",
-                            color: Colors.deepOrangeAccent),
-                        CustomButton(
-                            icon: Icons.play_circle_fill,
-                            label: "スポーツ",
-                            color: Colors.cyan),
-                      ];
-                      return buttons[index];
-                    },
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text("急上昇動画",
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 180,
-              child: Image.network(
-                'https://cgsc.info/cgsc2020/wp-content/uploads/2020/12/image.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-            ListTile(
-              title: Text(
-                '"This is ARASHI LIVE 2020.12.31" Digest Movie"',
-                style: TextStyle(color: Colors.white),
-              ),
-              subtitle: Text(
-                'ARASHI・127万 回視聴・1日前',
-                style: TextStyle(color: Colors.white54),
-              ),
-              leading: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Icon(
-                    Icons.circle,
-                    size: 60,
-                    color: Colors.white,
-                  ),
-                  const Text(
-                    'ARASHI',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text("急上昇動画",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 180,
-              child: Image.network(
-                'https://cgsc.info/cgsc2020/wp-content/uploads/2020/12/image.png',
-                fit: BoxFit.cover,
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: _MovieData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 180,
+                        child: Image.network(
+                          _MovieData[index].imagePath,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          _MovieData[index].title,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        subtitle: Text(
+                          _MovieData[index].subTitle,
+                          style: TextStyle(color: Colors.white54),
+                        ),
+                        leading: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              size: 60,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
