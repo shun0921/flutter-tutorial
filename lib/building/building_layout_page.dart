5b97f509d0820fdf8b98806217f239923bdeadac
菊地駿 <77576203+shun0921@users.noreply.github.com>
2023-11-16 10:07:54 +0900

    Update building_layout_page.dart

--- lib/building/building_layout_page.dart
+++ lib/building/building_layout_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BuildingLayoutPage extends StatelessWidget {
-  const BuildingLayoutPage({Key? key}) : super(key: key);
+  const BuildingLayoutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
